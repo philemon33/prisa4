fa6994c05ce9c415dc9f06727a5ee5cea26bd791
Vincent Chartier <vicharti@microsoft.com>
2016-03-29 14:17:09 +0200

    Angular2 first

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var http = require('http')
+//var http = require('http')
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Airbus Toulouse\n');
-}).listen(port);
+//http.createServer(function(req, res) {
+//  res.writeHead(200, { 'Content-Type': 'text/plain' });
+//  res.end('Hello Airbus Toulouse\n');
+//}).listen(port);
+
+var express = require('express');
+var server = express();
+//server.configure(function(){
+  //server.use('/media', express.static(__dirname + '/media'));
+  server.use(express.static(__dirname + '/'));
+//});
+
+server.listen(port);
+console.log('Server listening on port ' + port);
